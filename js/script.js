2f7624c9bf0fbdc49e23df097868002c25f4cc01
jihyun-works <jhk.jihyunkim@gmail.com>
2026-09-14 23:22:11 +0900

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('.submenu').hide();
-    $('.gnb li a').mouseenter(function(){
-        $('.submenu').stop().slideDown()
-    });
-    $("#nav").mouseleave(function(){
-        $('.submenu').stop().slideUp()
-    });
-});
